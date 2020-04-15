d0f6cd986e20c17b87b97d654408ebccf308db1a
Christos Katsenos <ckat@intracom-telecom.com>
2020-04-15 15:04:51 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace PerformanceMeter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblColumns = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRows = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMessageColumn = new System.Windows.Forms.Label();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblEndpointsNumber = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioAverage = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(13, 13);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFile.TabIndex = 0;
+            this.btnLoadFile.Text = "Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(94, 18);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(16, 13);
+            this.lblFileName.TabIndex = 1;
+            this.lblFileName.Text = "---";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(855, 320);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(651, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Columns:";
+            // 
+            // lblColumns
+            // 
+            this.lblColumns.AutoSize = true;
+            this.lblColumns.Location = new System.Drawing.Point(701, 24);
+            this.lblColumns.Name = "lblColumns";
+            this.lblColumns.Size = new System.Drawing.Size(16, 13);
+            this.lblColumns.TabIndex = 4;
+            this.lblColumns.Text = "---";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(779, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rows:";
+            // 
+            // lblRows
+            // 
+            this.lblRows.AutoSize = true;
+            this.lblRows.Location = new System.Drawing.Point(823, 22);
+            this.lblRows.Name = "lblRows";
+            this.lblRows.Size = new System.Drawing.Size(16, 13);
+            this.lblRows.TabIndex = 6;
+            this.lblRows.Text = "---";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Performance log in column:";
+            // 
+            // lblMessageColumn
+            // 
+            this.lblMessageColumn.AutoSize = true;
+            this.lblMessageColumn.Location = new System.Drawing.Point(154, 365);
+            this.lblMessageColumn.Name = "lblMessageColumn";
+            this.lblMessageColumn.Size = new System.Drawing.Size(16, 13);
+            this.lblMessageColumn.TabIndex = 8;
+            this.lblMessageColumn.Text = "...";
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAnalyze.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.btnAnalyze.Location = new System.Drawing.Point(654, 369);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(214, 38);
+            this.btnAnalyze.TabIndex = 9;
+            this.btnAnalyze.Text = "Analyze";
+            this.btnAnalyze.UseVisualStyleBackColor = false;
+            this.btnAnalyze.Visible = false;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(508, 382);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Endpoints found:";
+            // 
+            // lblEndpointsNumber
+            // 
+            this.lblEndpointsNumber.AutoSize = true;
+            this.lblEndpointsNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lblEndpointsNumber.Location = new System.Drawing.Point(615, 378);
+            this.lblEndpointsNumber.Name = "lblEndpointsNumber";
+            this.lblEndpointsNumber.Size = new System.Drawing.Size(21, 20);
+            this.lblEndpointsNumber.TabIndex = 11;
+            this.lblEndpointsNumber.Text = "...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 429);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(853, 200);
+            this.textBox1.TabIndex = 12;
+            // 
+            // radioAverage
+            // 
+            this.radioAverage.AutoSize = true;
+            this.radioAverage.Checked = true;
+            this.radioAverage.Location = new System.Drawing.Point(134, 393);
+            this.radioAverage.Name = "radioAverage";
+            this.radioAverage.Size = new System.Drawing.Size(65, 17);
+            this.radioAverage.TabIndex = 13;
+            this.radioAverage.TabStop = true;
+            this.radioAverage.Text = "Average";
+            this.radioAverage.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(215, 393);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Maximum";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 393);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Calculate and show:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 650);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioAverage);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblEndpointsNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.lblMessageColumn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblRows);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblColumns);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnLoadFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "PerformanceMeter";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblColumns;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRows;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMessageColumn;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblEndpointsNumber;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioAverage;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace PerformanceMeter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, int> endPointsTime = new Dictionary<string, int>();
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            lblEndpointsNumber.Text = "";
+
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open csv File";
+            theDialog.Filter = "csv files|*.csv";
+            //theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                String fileName = theDialog.FileName.ToString();
+                lblFileName.Text = fileName;
+
+                try
+                {
+                    ReadCSV csv = new ReadCSV(fileName);
+
+                    try
+                    {
+                        dataGridView1.DataSource = csv.readCSV;
+
+                        //Count rows and columns
+                        lblRows.Text = dataGridView1.Rows.Count.ToString();
+                        lblColumns.Text = dataGridView1.ColumnCount.ToString();
+
+                        //Find message column
+                        lblMessageColumn.Text = FindMessageColumn(dataGridView1);
+                        if (lblMessageColumn.Text != "Not found!")
+                        {
+                            btnAnalyze.Visible = true;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(ex.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+
+        private String FindMessageColumn(DataGridView datagridView)
+        {
+            int i = 0;
+            foreach (DataGridViewColumn column in datagridView.Columns)
+            {
+                if (column.Name == "message")
+                {
+                    return i.ToString();
+                }
+                i++;
+            }
+            return "Not found!";
+        }
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            this.Update();
+            ClearMessage(dataGridView1);
+        }
+
+
+        private void ClearMessage(DataGridView datagridView)
+        {
+            String tmpMessage = "";
+            Int16 columnNumber = Int16.Parse(lblMessageColumn.Text);
+
+            String action = "";
+            String time = "";
+            Int16 timeInt = 0;
+
+            foreach (DataGridViewRow row in datagridView.Rows)
+            {
+                if (row.Cells[columnNumber].Value != null)
+                {
+                    tmpMessage = row.Cells[columnNumber].Value.ToString();
+                    tmpMessage = tmpMessage.Replace("##PERFORMANCE##", "");
+
+                    try
+                    {
+                        ///FIND VALUES
+                        JObject json = JObject.Parse(tmpMessage);
+
+                        action = (string)json["action"];
+                        time = (string)json["time"];
+                        timeInt = Int16.Parse(time);
+
+                        if (CheckIfExist(action))
+                        {
+                            UpdateEndpointTime(action, timeInt);
+                        }
+                        else
+                        {
+                            AddEndpointTime(action, timeInt);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.Message);
+                        //throw;
+                    }
+
+                }
+            }
+
+            CreateReport();
+
+        }
+
+        private Boolean CheckIfExist(string action)
+        {
+            if (endPointsTime.ContainsKey(action))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void UpdateEndpointTime (string action, int actiontime)
+        {
+            Int16 newTime;
+
+            //Get time value from dictionary
+            var currentTime = endPointsTime[action];
+
+            if (radioAverage.Checked)
+            {
+                //Calculate average value
+                newTime = (Int16)((currentTime + actiontime) / 2);
+            }
+            else
+            {
+                if (actiontime > currentTime) { 
+                    //Replace value with bigger
+                    newTime = (Int16)actiontime;
+                } else {
+                    newTime = (Int16)currentTime;
+                }
+            }
+
+            //Update value in dictionary
+            endPointsTime[action] = newTime;
+
+        }
+
+       
+        private void AddEndpointTime(string action, int time)
+        {
+            endPointsTime.Add(action, time);
+        }
+
+
+        private void CreateReport()
+        {
+            lblEndpointsNumber.Text = endPointsTime.Count.ToString();
+
+            //Sort Dictionary
+            var items = from pair in endPointsTime
+                        orderby pair.Value descending
+                        select pair;
+
+            //Show results
+            textBox1.Lines = items.Select(x => x.Key + " : " + x.Value).ToArray();
+
+
+        }
+
+
+    }
+}
